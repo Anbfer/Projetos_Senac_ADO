8a6f069fd6f83b5818cc96ad9688e00e0521b26c
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2022-09-21 11:19:59 -0300

    Create 1_Atividade.java

---
+++ 1_Atividade.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Primeiro {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+				
+		System.out.println("Digite o valor de N1: " );
+		float n1 = input.nextFloat();
+		System.out.println("Digite o valor de N2: ");
+		float n2 = input.nextFloat();
+		System.out.println("Digite o valor de N3: ");
+		float n3 = input.nextFloat();
+		
+		float soma = n1 + n2 + n3;
+		
+		System.out.println("O produto da soma dos valores é: " + soma);
+		
+	}
+}
