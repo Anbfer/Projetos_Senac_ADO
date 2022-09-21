2136b2e9bbd28d72be87f6740e44f81b42b2102a
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2022-09-21 15:46:38 -0300

    Update 6_Atividade.java

--- Atividades/6_Atividade.java
+++ Atividades/6_Atividade.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class exercicioDoisAdoTres {
+public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
          System.out.println("Insira o valor do lado A: ");
