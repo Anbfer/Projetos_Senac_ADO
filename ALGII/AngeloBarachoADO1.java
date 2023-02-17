f8585d01061bbf230462535c1a0dc2b9135d85df
Ângelo Baracho <92029180+Anbfer@users.noreply.github.com>
2023-02-16 22:07:59 -0300

    Add files via upload

---
+++ ALGII/AngeloBarachoADO1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ads2sem.angelobarachoado1;
+
+/**
+ *
+ * @author angelo.bferreira
+ */
+public class AngeloBarachoADO1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ ALGII/animalConstrutor.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ads2sem.angelobarachoado1;
+
+/**
+ *
+ * @author angelo.bferreira
+ */
+public class animalConstrutor {
+    private static String animal = "Albatroz";
+    private String especie;
+    private boolean penagem;
+    private String cor;
+    
+    
+}
